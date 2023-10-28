12c1fd040bd3ae06df4d0a17407d66d7a3228757
lavilag4 <lavilag4@miumg.edu.gt>
2023-10-28 08:00:26 -0600

    numero1

---
+++ HolaMundoGitHub/src/holamundogithub/HolaMundoGitHub.java
@@ -0,0 +1,11 @@
+
+package holamundogithub;
+
+
+public class HolaMundoGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo para gitHub");
+    }
+    
+}
